78ccf30b127e59b4e7e869cabc777d6c15b3a3be
wenqiang.li <wenqiang.li@renren-inc.com>
2020-06-12 21:57:27 +0800

    行为监控demo页面

--- example/basic/app.js
+++ example/basic/app.js
@@ -1,12 +1,34 @@
+
+/**
+ * VueDataAc 配置
+ * */
 var OPTIONS = {
   useImgSend: false,
   selector: 'input,textArea',
 }
 Vue.use(VueDataAc, OPTIONS)
+
+
+/**
+ * vuerouter 测试数据
+ * */
+var Home = { template: '<div>home page</div>' }
+var Login = { template: '<div>login page</div>' }
+var User = { template: '<div>user page</div>' }
+
+var routes = [
+  { path: '/home', component: Home },
+  { path: '/login', component: Login },
+  { path: '/user', component: User },
+]
+const router = new VueRouter({
+  routes: routes
+})
 /**
  * 默认实例
  * */
 var app = new Vue({
+  router,
   el: '#app',
   data: {
     inputValue: '',
@@ -16,7 +38,17 @@ var app = new Vue({
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    /**
+     * 自定义事件上报
+     * */
+    customEvent: function(){
+      this.$vueDataAc.setCustomAc({
+        cusKey: 'clickAnyButton',
+        cusVal: 'click button ...'
+      })
+    }
+  },
   components: {},
   created: function(){
   },
